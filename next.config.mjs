606421e933214225ae3b987ac3e993eba11eb8e3
Syeda Umme Ayman <aymanshoity@gmail.com>
2024-07-17 11:36:32 +0600

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   images: {
+      remotePatterns: [
+       
+        {
+          protocol: 'https',
+          hostname: 'i.ibb.co', // replace with the correct hostname if different
+          port: '',
+          pathname: '/**',
+        },
+        
+      ],
+    },
+};
 
 export default nextConfig;
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello Nafisa apu</h1>
+    </div>
+  );
+}
